873a3e741ec3a8359fa59f0870bb4c7e74602d63
Thom204 <tmonnier@unal.edu.co>
2026-07-14 01:34:33 -0500

    njnnjn

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const mode = "NATS_FRIENDLY"
+const mode = "nTS_FRIENDLY"
 
 if (mode == "ATS_FRIENDLY") {
+    document.querySelectorAll("ul").forEach ( e => {e.style.listStyle = "disc";})
+
+
     document.querySelectorAll(".nats").forEach(e => {
         e.style.backgroundImage = "none";
+        console.log(e.innerHTML);
+        if (e.innerHTML.startsWith("<a")) {
+            const original = e.innerHTML;
+            e.innerHTML = String(e.id) +": \t"+ original;
+        }
+
     })
 }
