c70cc9ae5a93084dc18862d067ec3a7cca873d64
Thom204 <tmonnier@unal.edu.co>
2026-08-31 20:23:12 -0500

    added kewword stuffing for ats systems,  not playing fair anymore

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-const mode = "nTS_FRIENDLY"
-
-if (mode == "ATS_FRIENDLY") {
-    document.querySelectorAll("ul").forEach ( e => {e.style.listStyle = "disc";})
-
-
-    document.querySelectorAll(".nats").forEach(e => {
-        e.style.backgroundImage = "none";
-        console.log(e.innerHTML);
-        if (e.innerHTML.startsWith("<a")) {
-            const original = e.innerHTML;
-            e.innerHTML = String(e.id) +": \t"+ original;
+function tamper(skills, exp) {
+    const ctl = skills.split(", ");
+    const ctexp = exp.split(", ");
+    
+    document.querySelectorAll(`#habilidades > .skill`).forEach( p => {
+        if (ctl.length > 0) {
+            const ne = document.createElement("span");
+            ne.className = "skill";
+            ne.innerHTML = `<p style = "">${p.innerHTML}</p><p class = "invisi">${ctl.pop()}</p>`;
+            p.replaceWith(ne);
         }
+    })
 
+    document.querySelectorAll("#experiencia li").forEach( li => {
+        console.log(li);
+        if (ctexp.length > 0) {
+            const ni = document.createElement("li");
+            ni.innerHTML = `<p>${li.innerHTML}</p><p class = "invisi">${ctexp.pop()}</p>`;
+            li.replaceWith(ni);
+            console.log(li);
+        }
     })
 }
+
+
+document.querySelectorAll("ul").forEach ( e => {e.style.listStyle = "disc";})
+
+
+document.querySelectorAll(".nats").forEach(e => {
+    e.style.backgroundImage = "none";
+    console.log(e.innerHTML);
+    if (e.innerHTML.startsWith("<a")) {
+        const original = e.innerHTML;
+        e.innerHTML = String(e.id) +": \t"+ original;
+    }
+
+})
+
+const qstr = window.location.search;
+const qparams = new URLSearchParams(qstr);
+
+tamper(qparams.get("skkw"), qparams.get("expkw"));
+
